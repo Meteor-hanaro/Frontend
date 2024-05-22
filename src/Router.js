aeb822f73e2b9de86480d777755455b2a6ac6ab6
hayoung <hayounglala@jbnu.ac.kr>
2024-05-22 16:27:11 +0900

    feat: video connection, live data transaction

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import VideoPage from "./pages/VideoPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import FormPage from "./pages/FormPage";
@@ -10,6 +11,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/videoPage" element={<VideoPage />} />
         <Route path="/formPage" element={<FormPage />} />
         <Route path="/pb/profile" element={<ProfilePage />} />
       </Routes>
