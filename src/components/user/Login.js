24ab0ea9bda79785341f2d55596c22d5f0c2837f
kgw0124 <kgw0124@kakao.com>
2024-05-22 10:53:04 +0900

    feat: user login page 구현

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -5,38 +5,37 @@ function Login() {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-              <div className="d-flex justify-content-center py-4">
-                <a
-                  href="index.html"
-                  className="logo d-flex align-items-center w-auto"
-                >
-                  <img src="assets/img/logo.png" alt="" />
-                  <span className="d-none d-lg-block">NiceAdmin</span>
-                </a>
+              <div className="pt-4 pb-2">
+                <div className="d-flex flex-column align-items-center">
+                  <h5 className="card-title text-center pb-0 fs-4">
+                    우리는 더 나은 내일을 만듭니다.
+                  </h5>
+                  <p className="text-center small">
+                    We create a better tomorrow.
+                  </p>
+                </div>
               </div>
               {/* End Logo */}
               <div className="card mb-3">
                 <div className="card-body">
                   <div className="pt-4 pb-2">
-                    <h5 className="card-title text-center pb-0 fs-4">
-                      Login to Your Account
-                    </h5>
-                    <p className="text-center small">
-                      Enter your username &amp; password to login
-                    </p>
+                    <div className="d-flex flex-column align-items-center">
+                      <div className="logo d-flex align-items-center w-auto">
+                        <div className="card-title text-center pb-0 fs-4">
+                          <span>Gold Lounge</span>
+                        </div>
+                      </div>
+                      <p className="text-center small">
+                        Enter your ID &amp; PW to login
+                      </p>
+                    </div>
                   </div>
                   <form className="row g-3 needs-validation" noValidate="">
                     <div className="col-12">
                       <label htmlFor="yourUsername" className="form-label">
-                        Username
+                        ID
                       </label>
                       <div className="input-group has-validation">
-                        <span
-                          className="input-group-text"
-                          id="inputGroupPrepend"
-                        >
-                          @
-                        </span>
                         <input
                           type="text"
                           name="username"
@@ -51,7 +50,7 @@ function Login() {
                     </div>
                     <div className="col-12">
                       <label htmlFor="yourPassword" className="form-label">
-                        Password
+                        PW
                       </label>
                       <input
                         type="password"
@@ -65,43 +64,12 @@ function Login() {
                       </div>
                     </div>
                     <div className="col-12">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          name="remember"
-                          defaultValue="true"
-                          id="rememberMe"
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="rememberMe"
-                        >
-                          Remember me
-                        </label>
-                      </div>
-                    </div>
-                    <div className="col-12">
                       <button className="btn btn-primary w-100" type="submit">
                         Login
                       </button>
                     </div>
-                    <div className="col-12">
-                      <p className="small mb-0">
-                        Don't have account?{" "}
-                        <a href="pages-register.html">Create an account</a>
-                      </p>
-                    </div>
                   </form>
                 </div>
-              </div>
-              <div className="credits">
-                {/* All the links in the footer should remain intact. */}
-                {/* You can delete the links only if you purchased the pro version. */}
-                {/* Licensing information: https://bootstrapmade.com/license/ */}
-                {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ */}
-                Designed by{" "}
-                <a href="https://bootstrapmade.com/">BootstrapMade</a>
               </div>
             </div>
           </div>
