7948acb9057a3270e97f7363baa48b563a86e5f7
hayoung <hayounglala@jbnu.ac.kr>
2024-05-24 15:33:33 +0900

    feat: video view ui

---
+++ src/pages/video/VideoPage.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useRef } from 'react';
+import WebRTC from '../../WebRTC';
+
+const VideoPage = () => {
+    const [signaling, setSignaling] = useState(null);
+    const sendMessageRef = useRef(null);
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8888');
+        ws.onopen = () => {
+            console.log('Connected to signaling server');
+        };
+        setSignaling(ws);
+
+        return () => {
+            ws.close();
+        };
+    }, []);
+
+    const handleSendMessage = (sendMessage) => {
+        sendMessageRef.current = sendMessage;
+    };
+
+    return (
+        <div className="VideoPage">
+            <div id="videoContainer">
+                {signaling && (
+                    <WebRTC
+                        signaling={signaling}
+                        onSendMessage={handleSendMessage}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default VideoPage;
