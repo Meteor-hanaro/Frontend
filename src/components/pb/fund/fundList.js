088e03c40d99126ee2b8e01e18b5f95c3a0e7610
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-27 19:30:53 +0900

    Feat: Print all fund list in fundList.js

--- src/components/pb/fund/fundList.js
+++ src/components/pb/fund/fundList.js
@@ -8,15 +8,40 @@ function FundList() {
     const fetchData = async () => {
       try {
         axios.get("http://localhost:8080/api/fund/get").then((res) => {
-            setFund(res.data);
-            console.log(res.data);
+          setFund(res.data);
+          console.log(res.data);
         });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     };
     fetchData();
   }, []);
+
+    return (
+        <>
+        <div className="col-12" style={{ marginTop: "25px" }}>
+            <div className="card recent-sales overflow-auto">
+            <div className="card-body" style={{ padding: "20px" }}>
+                <table className="table table-borderless datatable">
+                <thead>
+                    <tr>
+                    <th scope="col">Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fund.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.name}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+        </>
+    );  
 }
 
 export default FundList;
