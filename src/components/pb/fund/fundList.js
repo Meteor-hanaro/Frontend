c78d3af5d95c907a5690de1e9dbcc81bf256774b
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-27 19:45:29 +0900

    Feat: Import fund detail onclick of fund name

--- src/components/pb/fund/fundList.js
+++ src/components/pb/fund/fundList.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 function FundList() {
   const [fund, setFund] = useState([]);
+  const [selectedFund, setSelectedFund] = useState(null);
+  const [fundDetails, setFundDetails] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,37 +20,54 @@ function FundList() {
     fetchData();
   }, []);
 
-    return (
-        <>
-        <div className="col-12" style={{ marginTop: "25px" }}>
-            <div className="card recent-sales overflow-auto">
-            <div className="card-body" style={{ padding: "20px" }}>
-                <table className="table table-borderless datatable">
-                <thead>
-                    <tr>
-                    <th scope="col">Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {fund.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.name}</td>
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
-            </div>
-            </div>
+  const handleItemClick = (item) => {
+    setSelectedFund(item);
+    fetchFundDetails(item.id); // assuming item has an id
+  };
+
+  const fetchFundDetails = async (id) => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/fund/securities/get`, {
+        params: {
+          id: id,
+        },
+      });
+      setFundDetails(response.data);
+      alert(JSON.stringify(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="col-12" style={{ marginTop: "25px" }}>
+        <div className="card recent-sales overflow-auto">
+          <div className="card-body" style={{ padding: "20px" }}>
+            <table className="table table-borderless datatable">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {fund.map((item, index) => (
+                  <tr key={index}>
+                    <td
+                      onClick={() => handleItemClick(item)}
+                      className="btn btn-link"
+                    >
+                      {item.name}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        </>
-    );  
+      </div>
+    </>
+  );
 }
 
 export default FundList;
-
-// useEffect(() => {
-//     axios.get("http://localhost:8080/api").then((res) => {
-//       setData(res.data);
-//       console.log(data);
-//     });
-//   });
