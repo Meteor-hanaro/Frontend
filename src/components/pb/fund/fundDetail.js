80754dd60a0d810c8b6e4fe9cbd5593b78c3b7ff
Kwak-JunYoung <klop9090@g.skku.edu>
2024-05-28 11:21:50 +0900

    Feat: Obtain fund detail

--- src/components/pb/fund/fundDetail.js
+++ src/components/pb/fund/fundDetail.js
@@ -1,4 +1,34 @@
-function FundDetail({selectedFund}) {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function FundDetail({ selectedFund }) {
+  const [selectedFundDetail, setSelectedFundDetail] = useState([]);
+
+  useEffect(() => {
+    if (!selectedFund) {
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        axios
+          .get("http://localhost:8080/api/fund/securities/get", {
+            params: {
+              id: selectedFund.id,
+            },
+          })
+          .then((res) => {
+            console.log(res.data);
+            setSelectedFundDetail(res.data);
+            
+          });
+      } catch (error) {
+        console.log(error);
+      }
+      
+    };
+    fetchData();
+  }, [selectedFund]);
+
   if (!selectedFund) {
     return <div className="fund-detail">펀드를 선택하세요</div>;
   }
@@ -13,17 +43,18 @@ function FundDetail({selectedFund}) {
         <thead>
           <tr>
             <th>자산구분</th>
-            <th>금액(백만)</th>
             <th>비중(%)</th>
           </tr>
         </thead>
         <tbody>
           {/* 샘플 데이터, 실제 데이터로 교체 필요 */}
-          <tr>
-            <td>주식</td>
-            <td>11,200</td>
-            <td>30.43</td>
-          </tr>
+          {selectedFundDetail &&
+            selectedFundDetail.map((data, index) => (
+              <tr key={index}>
+                <td>{data.security.name}</td>
+                <td>{data.fundSecurityPercentage}</td>
+              </tr>
+            ))}
           {/* 추가 데이터 행들 */}
         </tbody>
       </table>
