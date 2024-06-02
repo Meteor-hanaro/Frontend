416d313e59d84f1e2f7b1686256739f18daffe0e
hayoung <hayounglala@jbnu.ac.kr>
2024-06-02 23:58:05 +0900

    vip's password check

--- src/components/user/Main.js
+++ src/components/user/Main.js
@@ -1,27 +1,29 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import auth from '../../auth';
 import Modal from 'react-modal';
-import { LoginContext } from "../../contexts/LoginContextProvider";
+import axios from 'axios';
 
 function Main() {
-  const [data, setData] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [vipId, setVipId] = useState('');
   const [vipPwd, setVipPwd] = useState('');
   const [pbId, setPbId] = useState('');
-
   const [password, setPassword] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/api").then((res) => {
-      setData(res.data);
-      console.log(data);
-      setVipId(res.data.vipInfo.vipId);
-      setVipPwd(res.data.vipInfo.vipPwd);
-      setPbId(res.data.pbInfo.pbId);
-    });
-  });
+    auth
+      .get('http://127.0.0.1:8080/api/vip/main')
+      .then((res) => {
+        console.log(res);
+        setVipId(res.data.vipInfo.vipId);
+        setVipPwd(res.data.vipInfo.password);
+        setPbId(res.data.pbInfo.pbId);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -37,20 +39,29 @@ function Main() {
   };
 
   const handlePasswordSubmit = (e) => {
-    axios.post('http://localhost:8080/api/main/pwdcheck', {
-      password: vipPwd,
-      writtenPwd: e.target.value
+    axios.post('http://127.0.0.1:8080/api/vip/main/pwdcheck', {
+      pwd: vipPwd,
+      writtenPwd: password
     })
-    
-    if (password === '1111') {
-      setIsAuthenticated(true);
-      closeModal();
-      alert('확인되었습니다. 상담실로 입장합니다.');
-      window.open(`./videoPage/pbId=${pbId}&vipId=${vipId}`, '_blank', 'noopener,noreferrer');
-    } else {
-      alert('비밀번호가 틀렸습니다. 다시 입력해주세요.');
-      setPassword('');
-    }
+    .then(response => {
+      if (response.data) {
+        setIsAuthenticated(true);
+        closeModal();
+        alert('확인되었습니다. 상담실로 입장합니다.');
+        window.open(
+          `./videoPage/pbId=${pbId}&vipId=${vipId}`,
+          '_blank',
+          'noopener,noreferrer'
+        );
+      } else {
+        alert('비밀번호가 틀렸습니다. 다시 입력해주세요.');
+        setPassword('');
+      }
+    })
+    .catch(error => {
+      console.error();
+    });
+
   };
   return (
     <>
@@ -58,33 +69,33 @@ function Main() {
         id="main"
         className="main"
         style={{
-          padding: "45px",
-          marginLeft: "5%",
-          marginRight: "5%",
+          padding: '45px',
+          marginLeft: '5%',
+          marginRight: '5%',
           height: `calc(100vh - 60px)`,
         }}
       >
         <div
           className="pagetitle alignHorizontal"
           style={{
-            height: "100%",
+            height: '100%',
           }}
         >
           <div
             className="alignVertical"
             style={{
-              width: "35%",
+              width: '35%',
             }}
           >
             <div
               className="card info-card alignVertical"
-              style={{ height: "47%", padding: "7% 7%" }}
+              style={{ height: '47%', padding: '7% 7%' }}
             >
               <span
                 style={{
-                  fontSize: "20px",
-                  fontWeight: "600",
-                  color: "#D7B863",
+                  fontSize: '20px',
+                  fontWeight: '600',
+                  color: '#D7B863',
                 }}
               >
                 VIP
@@ -92,34 +103,34 @@ function Main() {
               <div
                 className="alignHorizontal"
                 style={{
-                  marginTop: "5%",
+                  marginTop: '5%',
                 }}
               >
-                <div style={{ fontSize: "26px", fontWeight: "540" }}>
+                <div style={{ fontSize: '26px', fontWeight: '540' }}>
                   <div>
-                    <span style={{ fontSize: "35px", fontWeight: "750" }}>
-                      이진만{" "}
+                    <span style={{ fontSize: '35px', fontWeight: '750' }}>
+                      이진만{' '}
                     </span>
-                    님<div style={{ marginTop: "3px" }}>건행하세요 😊</div>
+                    님<div style={{ marginTop: '3px' }}>건행하세요 😊</div>
                   </div>
                 </div>
                 <div>
                   <div>
                     <span
                       style={{
-                        fontSize: "22px",
-                        fontWeight: "550",
-                        color: "#009476",
+                        fontSize: '22px',
+                        fontWeight: '550',
+                        color: '#009476',
                       }}
                     >
                       안정투자형
                     </span>
                     <div
                       style={{
-                        marginTop: "0.5px",
-                        fontSize: "15px",
-                        fontWeight: "700",
-                        color: "#5F5F5F",
+                        marginTop: '0.5px',
+                        fontSize: '15px',
+                        fontWeight: '700',
+                        color: '#5F5F5F',
                       }}
                     >
                       최근 검사일 0000-00-00
@@ -130,7 +141,7 @@ function Main() {
               <button
                 type="button"
                 className="enterButton"
-                style={{ marginBottom: "0px" }}
+                style={{ marginBottom: '0px' }}
                 onClick={openModal}
               >
                 상담 바로 입장하기
@@ -140,74 +151,87 @@ function Main() {
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 contentLabel="Password Modal"
-                className="consultingModal"  
-                overlayClassName="consultingModalOverlay"  
+                className="consultingModal"
+                overlayClassName="consultingModalOverlay"
               >
-                <h4>상담실 입장을 위해 비밀번호 확인이 필요합니다.</h4> <br/>
-                <input 
+                <h4>상담실 입장을 위해 비밀번호 확인이 필요합니다.</h4> <br />
+                <input
                   id="inputPwd"
-                  type="password" 
-                  value={password} 
-                  onChange={handlePasswordChange} 
-                /> <br/> <br/>
-                <button id="buttonCheck" className="btn btn-primary" onClick={handlePasswordSubmit}>확인</button>
-                <button id="buttonCancel" className="btn btn-primary" onClick={closeModal}>취소</button>
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />{' '}
+                <br /> <br />
+                <button
+                  id="buttonCheck"
+                  className="btn btn-primary"
+                  onClick={handlePasswordSubmit}
+                >
+                  확인
+                </button>
+                <button
+                  id="buttonCancel"
+                  className="btn btn-primary"
+                  onClick={closeModal}
+                >
+                  취소
+                </button>
               </Modal>
             </div>
             <div
               className="card info-card alignVertical"
-              style={{ height: "47%", padding: "7% 7%", marginBottom: "0px" }}
+              style={{ height: '47%', padding: '7% 7%', marginBottom: '0px' }}
             >
               <span
                 style={{
-                  fontSize: "20px",
-                  fontWeight: "600",
-                  color: "#D7B863",
+                  fontSize: '20px',
+                  fontWeight: '600',
+                  color: '#D7B863',
                 }}
               >
                 PB
               </span>
-              <div style={{ fontSize: "23px", fontWeight: "540" }}>
-                담당{" "}
-                <span style={{ fontSize: "26px", fontWeight: "750" }}>
+              <div style={{ fontSize: '23px', fontWeight: '540' }}>
+                담당{' '}
+                <span style={{ fontSize: '26px', fontWeight: '750' }}>
                   곽준영
                 </span>
               </div>
               <div
                 className="alignHorizontal"
                 style={{
-                  height: "70%",
-                  alignItems: "center",
+                  height: '70%',
+                  alignItems: 'center',
                 }}
               >
                 <div>
                   <img
-                    src={process.env.PUBLIC_URL + "/assets/img/profile-img.jpg"}
+                    src={process.env.PUBLIC_URL + '/assets/img/profile-img.jpg'}
                     alt="Profile"
                     className="rounded-circle"
                   />
                 </div>
                 <div
                   style={{
-                    fontSize: "15px",
-                    fontWeight: "550",
-                    color: "#5F5F5F",
-                    marginLeft: "auto",
-                    marginRight: "auto 0px",
+                    fontSize: '15px',
+                    fontWeight: '550',
+                    color: '#5F5F5F',
+                    marginLeft: 'auto',
+                    marginRight: 'auto 0px',
                   }}
                 >
                   <div>
                     <span>bboyami@hana.co.kr</span>
                     <div
                       style={{
-                        marginTop: "0.5px",
+                        marginTop: '0.5px',
                       }}
                     >
                       010-0000-0000
-                    </div>{" "}
+                    </div>{' '}
                     <div
                       style={{
-                        marginTop: "0.5px",
+                        marginTop: '0.5px',
                       }}
                     >
                       퇴근 원츄
@@ -217,16 +241,16 @@ function Main() {
               </div>
             </div>
           </div>
-          <div style={{ marginLeft: "5%", width: "60%", height: "100%" }}>
+          <div style={{ marginLeft: '5%', width: '60%', height: '100%' }}>
             <div
               className="card info-card"
-              style={{ height: "100%", padding: "4% 4%" }}
+              style={{ height: '100%', padding: '4% 4%' }}
             >
               <span
                 style={{
-                  fontSize: "20px",
-                  fontWeight: "600",
-                  color: "#D7B863",
+                  fontSize: '20px',
+                  fontWeight: '600',
+                  color: '#D7B863',
                 }}
               >
                 상담 이력
