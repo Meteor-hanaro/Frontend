22e3e228d9cedbffd667c1b840a81f17b2ca65bd
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-02 15:53:49 +0900

    Feat: Enable selection between table and graph

--- src/pages/pb/PortfolioPage.js
+++ src/pages/pb/PortfolioPage.js
@@ -1,15 +1,26 @@
 // 손님 포트폴리오 관리 페이지
+import { useState } from "react";
 
 import SideBar from "../../components/pb/Sidebar";
 import PortfolioTable from "../../components/pb/portfolio/portfolioTable";
+import PortfolioGraph from "../../components/pb/portfolio/portfolioGraph";
 
 function PortfolioPage() {
+  const [showGraph, setShowGraph] = useState(false);
+
+  const toggleView = () => {
+    setShowGraph(!showGraph);
+  };
+
   return (
     <div className="fund-page">
       <SideBar />
       <main className="main" id="main" style={{ width: "100%" }}>
+        <button onClick={toggleView} className="toggle-button">
+          {showGraph ? "Show Table" : "Show Graph"}
+        </button>
         <div className="main-content">
-          <PortfolioTable />
+          {showGraph ? <PortfolioGraph /> : <PortfolioTable />}
           {/* <AmountInput /> */}
         </div>
       </main>
