d3632e9ef11b5700983e43fd980c6a1c1a32a64e
hayoung <hayounglala@jbnu.ac.kr>
2024-06-03 21:33:03 +0900

    limit-on-the-number-of-people

--- src/config/signaling-server.js
+++ src/config/signaling-server.js
@@ -6,11 +6,56 @@ const wsspb = new WebSocket.Server({ port: 8890 }); // progrss bar
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+
+
+    const data = JSON.parse(message);
+
+    switch (data.type) {
+        case 'join':
+            const room = data.room;
+            const isPb = data.isPb;
+
+            if (!rooms[room]) {
+                rooms[room] = { pb: null, users: [] };
+            }
+ 
+            const roomInfo = rooms[room];
+            if (isPb) {
+                if (roomInfo.pb) {
+                    ws.send(JSON.stringify({ type: 'room_full', message: 'PB already in the room.' }));
+                } else {
+                    roomInfo.pb = ws;
+                    ws.room = room;
+                    ws.isPb = true;
+                    ws.send(JSON.stringify({ type: 'joined', room }));
+                    broadcastToRoom(room, { type: 'user_joined', isPb: true });
+                }
+            } else {
+                if (roomInfo.users.length >= 1) {
+                    ws.send(JSON.stringify({ type: 'room_full', message: 'Room already has a user.' }));
+                } else {
+                    roomInfo.users.push(ws);
+                    ws.room = room;
+                    ws.isPb = false;
+                    ws.send(JSON.stringify({ type: 'joined', room }));
+                    broadcastToRoom(room, { type: 'user_joined', isPb: false });
+                }
+            }
+            break;
+
+        case 'leave':
+            leaveRoom(ws);
+            break;
+
+        default:
+            break;
+    }
+
+    // wss.clients.forEach((client) => {
+    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+    //     client.send(message);
+    //   }
+    // });
   });
 
   ws.on("close", () => {
