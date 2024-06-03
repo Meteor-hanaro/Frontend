5e75f1afe6c855c83e0b61177a9eed87c662950c
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-03 11:02:29 +0900

    Feat: Redirection to main page from portfolio page when not logged in

--- src/pages/pb/PortfolioPage.js
+++ src/pages/pb/PortfolioPage.js
@@ -4,9 +4,22 @@ import { useState } from 'react';
 import SideBar from '../../components/pb/Sidebar';
 import PortfolioTable from '../../components/pb/portfolio/portfolioTable';
 import PortfolioGraph from '../../components/pb/portfolio/portfolioGraph';
+import Header from '../../components/common/Header';
+
+import { LoginContext } from '../../contexts/LoginContextProvider';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function PortfolioPage() {
   const [showGraph, setShowGraph] = useState(false);
+  const { isLogin } = useContext(LoginContext);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLogin) {
+      navigate('/pb');
+    }
+  }, []);
 
   const toggleView = () => {
     setShowGraph(!showGraph);
@@ -14,6 +27,7 @@ function PortfolioPage() {
 
   return (
     <div className='fund-page'>
+      <Header />
       <SideBar />
       <main className='main' id='main' style={{ width: '100%' }}>
         <button
