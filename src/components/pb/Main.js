f32c9121bfae4bb5c434ced4a500e646ce196400
kgw0124 <kgw0124@kakao.com>
2024-06-04 14:13:24 +0900

    feat: /pb/main 검색바 css 수정

--- src/components/pb/Main.js
+++ src/components/pb/Main.js
@@ -42,54 +42,60 @@ function Main() {
   return (
     <>
       <main
-        id="main"
-        className="main"
+        id='main'
+        className='main'
         style={{
           padding: '45px',
           height: `calc(100vh - 60px)`,
         }}
       >
-        <div className="pagetitle">
+        <div className='pagetitle'>
           <h1>사용자 목록</h1>
         </div>
         {/* Search Bar */}
-        <div className="search-bar" style={{ marginTop: '25px' }}>
-          <form
-            className="search-form d-flex align-items-center"
-            method="POST"
-            action="#"
-          >
-            <input
-              type="text"
-              name="query"
-              placeholder="Search"
-              title="Enter search keyword"
-            />
-            <button type="submit" title="Search">
-              <i className="bi bi-search" />
-            </button>
-          </form>
+        <div
+          className='alignHorizontal search-bar'
+          style={{ marginTop: '25px' }}
+        >
+          <select className='datatable-selector'>
+            <option>전체</option>
+            <option>STABLE</option>
+            <option>CONSERVATIVE</option>
+            <option>NEUTRAL</option>
+            <option>GROWTH</option>
+            <option>AGGRESSIVE</option>
+          </select>
+          <input
+            className='search-form'
+            type='text'
+            name='query'
+            placeholder='Search'
+            title='Enter search keyword'
+          />
+          <button className='search-button' type='submit' title='Search'>
+            <i className='bi bi-search' />
+          </button>
         </div>
         {/* End Search Bar */}
         {/* Recent Sales */}
-        <div className="col-12" style={{ marginTop: '25px' }}>
-          <div className="card recent-sales overflow-auto">
-            <div className="card-body" style={{ padding: '20px' }}>
-              <table className="table table-borderless datatable">
+        <div className='col-12' style={{ marginTop: '25px' }}>
+          <div className='card recent-sales overflow-auto'>
+            <div className='card-body' style={{ padding: '20px' }}>
+              <table className='table table-borderless datatable'>
                 <thead>
                   <tr>
-                    <th className="col-1">No</th>
-                    <th className="col-1">Status</th>
-                    <th className="col-1">Name</th>
-                    <th className="col-2">Risk Tolerance</th>
-                    <th className="col-3">Final Consultation Date</th>
-                    <th className="col-5">Portfolio</th>
+                    <th className='col-1'>No</th>
+                    <th className='col-1'>Status</th>
+                    <th className='col-1'>Name</th>
+                    <th className='col-2'>Risk Tolerance</th>
+                    <th className='col-3'>Final Consultation Date</th>
+                    <th className='col-5'>Portfolio</th>
                   </tr>
                 </thead>
                 <tbody>
                   {state.map((item, index) => (
                     <tr key={index}>
-                      <th scope="row">
+                      <th scope='row'>
                         <a
                           href={`#${vip[index].vipId}`}
                         >{`#${vip[index].vipId}`}</a>
@@ -108,29 +114,29 @@ function Main() {
                       <td>{vip[index].consultDate}</td>
                       <td>
                         <button
-                          type="button"
-                          className="pbBtn"
+                          type='button'
+                          className='pbBtn'
                           onClick={() => checkPortfolio(vip[index].vipId)}
                         >
-                          <i class="bi bi-clipboard2-data"></i>
+                          <i class='bi bi-clipboard2-data'></i>
                           포트폴리오
                         </button>
                         <Link to={`/pb/suggestion/${item.vipId}`}>
                           <button
-                            type="button"
-                            className="pbBtn"
+                            type='button'
+                            className='pbBtn'
                             style={{ marginLeft: '20px' }}
                           >
-                            <i className="bi bi-clipboard2-check"></i>
+                            <i className='bi bi-clipboard2-check'></i>
                             &nbsp; 제안서
                           </button>
                         </Link>
                         <button
-                          type="button"
-                          className="pbBtn"
+                          type='button'
+                          className='pbBtn'
                           style={{ marginLeft: '20px' }}
                         >
-                          <i className="bi bi-person-rolodex"></i>
+                          <i className='bi bi-person-rolodex'></i>
                           &nbsp; 상담 신청
                         </button>
                       </td>
