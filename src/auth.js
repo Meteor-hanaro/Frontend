0da89b127a3184bde650898b68fa7387ee1d6a52
kgw0124 <kgw0124@kakao.com>
2024-06-07 17:33:41 +0900

    feat: 로그인하지 않으면 /로 튕기도록 구현

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,7 @@ const auth = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
 });
 
+// jwt token 포함해서 request 생성
 auth.interceptors.request.use(
   (config) => {
     config.headers['accessToken'] = localStorage.getItem('accessToken');
@@ -12,6 +13,20 @@ auth.interceptors.request.use(
   },
   (error) => {
     console.log(error);
+  }
+);
+
+// jwt token 관련 response 처리
+auth.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const code = error.response.status;
+    console.log(code);
+    if (code === 401) {
+      window.location.href = '/';
+    }
   }
 );
 
