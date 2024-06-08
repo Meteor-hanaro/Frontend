635a2045e51c3bf912bd9a44998af66777e092e7
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-08 15:58:25 +0900

    Feat: View and features for addition of suggestion

--- src/components/pb/suggestion/SuggestionAdd.js
+++ src/components/pb/suggestion/SuggestionAdd.js
@@ -1,62 +1,76 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SuggestionCard from './SuggestionCard';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-
-function SuggestionList({ id }) {
+function SuggestionAdd() {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const [suggestionItemList, setSuggestionItemList] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const nameResizing = (data) => {
-    return data.length > 10 ? data.substring(0, 10) + '...' : data;
-  };
-
-  const makingChartData = (data) =>
-    data &&
-    data.suggestionItems.map((item) => ({
-      value: item.suggestionValue,
-      name: nameResizing(item.suggestionItemName),
-    }));
-
-  const handleAddSuggestion = () => {
-    navigate('/add-suggestion');
-  }
-
+  const [loading, setLoading] = useState(true);
+  const [suggestionObtainDto, setSuggestionObtainDto] = useState({});
+  const { suggestionId } = location.state || {}; // 전달된 상태에서 suggestionId 추출
+  console.log(suggestionId);
   useEffect(() => {
     axios
       .get(
-        `http://${process.env.REACT_APP_BESERVERURI}/api/suggestion/extract?userId=` +
-          id
+        `http://${process.env.REACT_APP_BESERVERURI}/api/suggestion/obtain?suggestion_id=` +
+          suggestionId,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json', // 서버가 JSON 형식으로 응답하도록 요청
+          },
+        }
       )
       .then((res) => {
-        // console.log(res.data);
-        setSuggestionItemList(res.data);
-        setLoading(true);
+        console.log(res.data);
+        setSuggestionObtainDto(res.data);
+        setLoading(false);
       })
       .catch((e) => console.log(e));
   }, []);
 
-  if (!loading) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className='suggestion-list-page vertical-align'>
-      <div id='main' className='d-flex'>
-        {suggestionItemList &&
-          suggestionItemList.suggestionItems.map((item, index) => (
-            <SuggestionCard
-              data={makingChartData(item)}
-              name={item.suggestionName}
-            />
-          ))}
+    <div>
+      <div id='main'>
+        <h3>{suggestionObtainDto.vipName}님에게 드릴 제안입니다.</h3>
+        <table className='suggestion-add-table'>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>종목명</th>
+              <th>가입일</th>
+              <th>투입금액</th>
+              <th>현재가치</th>
+              <th>신규투입</th>
+            </tr>
+          </thead>
+          <tbody>
+            {suggestionObtainDto.suggestionItems.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.fundName}</td>
+                <td>
+                  {item.fundInitDate[0]}-{item.fundInitDate[1]}-
+                  {item.fundInitDate[2]}
+                </td>
+                <td>{item.fundInitValue.toLocaleString('ko-KR')}</td>
+                <td>{item.fundCurrentValue.toLocaleString('ko-KR')}</td>
+                <td>
+                  <input type='number' className='newInput' />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <button className='btn btn-success add-suggestion-btn'>Add suggestion</button> 
     </div>
   );
 }
 
-export default SuggestionList;
+export default SuggestionAdd;
