6189db08ca62d8e274eb32bda0a611dda8746f2e
tkdalsss <lsm991229@naver.com>
2024-06-08 13:48:20 +0900

    feature/system_admin_page_advanced

--- src/pages/system_admin/AdminPage.js
+++ src/pages/system_admin/AdminPage.js
@@ -1,6 +1,7 @@
 import Header from '../../components/system_admin/Header';
-import Main from '../../components/system_admin/Main';
+// import Main from '../../components/system_admin/Main';
 import { useState, useEffect } from 'react';
+import Main from '../../components/etc/Main';
 
 function AdminPage() {
   const [infoNumber, setInfoNumber] = useState(0);
@@ -12,7 +13,8 @@ function AdminPage() {
   return (
     <>
       <Header setInfoNumber={setInfoNumber} />
-      <Main infoNumber={infoNumber} />
+      {/* <Main infoNumber={infoNumber} /> */}
+      <Main />
     </>
   );
 }
