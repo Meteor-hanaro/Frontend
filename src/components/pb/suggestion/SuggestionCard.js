89f821584b62d88fda3db8c2afe1e41c1f948915
Kwak-JunYoung <klop9090@g.skku.edu>
2024-06-09 22:27:07 +0900

    Feat: Modification of suggestion onclick button

--- src/components/pb/suggestion/SuggestionCard.js
+++ src/components/pb/suggestion/SuggestionCard.js
@@ -1,21 +1,29 @@
 import TrafficChart from '../../common/chart/TrafficChart';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from 'react';
+
 
 function SuggestionCard({ id, data, name }) {
+  const navigate = useNavigate();
 
+  const [suggestionId, setSuggestionId] = useState(id);
   const handleDelete = () => {
     // 삭제 로직
-    axios.get('http://localhost:8080/api/suggestion/remove?suggestion_id=' + id)
+    axios
+      .get('http://localhost:8080/api/suggestion/remove?suggestion_id=' + id)
       .then((res) => {
         console.log(res.data);
         window.location.reload();
         alert('삭제되었습니다.');
       })
       .catch((e) => console.log(e));
-  }
-
-  console.log(data);
+  };
+  
+  const handleSeeDetail = () => {
+    navigate('/pb/suggestion/add', { state: { suggestionId: suggestionId } });
+  };
+  
   return (
     <div className='card mx-2' style={{ width: '40rem', position: 'relative' }}>
       <TrafficChart data={data} name={name} />
@@ -30,9 +38,9 @@ function SuggestionCard({ id, data, name }) {
         <p className='card-text'>
           2024년 06월 03일 13시 40분 35초에 저장한 포트폴리오 수정안입니다
         </p>
-        <Link to='#' className='btn btn-secondary'>
+        <button onClick={handleSeeDetail} className='btn btn-secondary'>
           See Detail
-        </Link>
+        </button>
       </div>
     </div>
   );
