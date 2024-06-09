dd1c3ecc413022f2cb2a34608b9e0d8f3d36746b
hayoung <hayounglala@jbnu.ac.kr>
2024-06-09 16:08:29 +0900

    1:1 consulting room

--- src/config/signaling-server.js
+++ src/config/signaling-server.js
@@ -6,6 +6,8 @@ const wsssl = new WebSocket.Server({ port: 8889 }); // suggestion list
 const wsspb = new WebSocket.Server({ port: 8890 }); // progress bar
 
 let participants = 0;
+// let usingVip = null;
+// let usingPb = null;
 
 wsscr.on('connection', (ws) => {
   ws.on('message', (message) => {
@@ -25,50 +27,118 @@ console.log(
   'WebSocket server for consult request is running on ws://localhost:8887'
 );
 
+
+let pbId = '';
+let vipId = '';
+let isVip = false;
+let isPb = false;
+let isOpen = false;
+let usingVip = false;
+let usingPb = false;
+let vipOverflow = false;
+let pbOverflow = false;
 wss.on('connection', (ws, req) => {
+
+  ws.on('message', message => {
+    const jsonString = message.toString();
+    const parsedObject = JSON.parse(jsonString);
+    console.log(parsedObject);
+
+    isOpen = parsedObject.isOpen;
+    isVip = parsedObject.isVip;
+    isPb = parsedObject.isPb;
+    vipId = parsedObject.vipId;
+    pbId = parsedObject.pbId;
+
+    if(isOpen && isVip) {
+      if(!usingVip) {
+        usingVip = true;
+        participants++;
+      } else {
+        vipOverflow = true;
+      }
+    }
+    else if(isOpen && isPb) {
+      if(!usingPb) {
+        usingPb = true;
+        participants++;
+      } else {
+        pbOverflow = true;
+      }
+    }
+    else if(!isOpen) {
+      if(!vipOverflow && !pbOverflow) {
+        participants--;
+        usingVip = false;
+        usingPb = false;
+      }
+      participants = participants < 0 ? 0 : participants;
+      vipOverflow = false;
+      pbOverflow = false;
+    }
+
+    ws.send(JSON.stringify({ count: participants, isVip, isPb, vipOverflow, pbOverflow })); 
+  }) 
+
   const channel = req.url.split('/').pop();
+  
+  console.log("channel", channel, vipId, pbId, isVip, isPb);
 
   ws.channel = channel;
 
-  participants++;
-  console.log('New connection. Participants:', participants);
+  console.log("url:", req.url);
+  
+  
+  // broadcastParticipants();
+  console.log('New connection. Participants:', participants, vipId, pbId, isVip, isPb, usingVip, usingPb);
 
-  // Send the current participants count to the new client
+  
   ws.send(JSON.stringify({ type: 'participants', count: participants }));
+  console.log("send msg");
+  
 
-  // Broadcast the updated participants count to all clients
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ type: 'participants', count: participants }));
-    }
-  });
+  
+  // wss.clients.forEach((client) => {
+  //   if (client.readyState === WebSocket.OPEN && client.channel === channel) {
+  //     try {
+  //       client.send(JSON.stringify({ type: 'participants', count: participants }));
+  //     } catch (error) {
+  //       console.error('Error broadcasting participants count:', error);
+  //     }
+  //   }
+  // });
 
   ws.on('close', () => {
-    participants--;
     console.log('Connection closed. Participants:', participants);
 
-    // Broadcast the updated participants count to all clients
     wss.clients.forEach((client) => {
       if (
         client !== ws &&
         client.readyState === WebSocket.OPEN &&
         client.channel === channel
       ) {
-        // client.send(JSON.stringify({ type: 'participants', count: participants }));
+        client.send(JSON.stringify({ type: 'participants', count: participants }));
 
-        client.send(message);
+        // client.send(message);
       }
     });
   });
 });
 
-wsssl.on('connection', (ws, req) => {
-  const channel = req.url.split('/').pop();
 
-  console.log(req.url);
-  ws.channel = channel;
+function broadcastParticipants() {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        client.send(JSON.stringify({ type: 'participants', count: participants }));
+      } catch (error) {
+        console.error('Error broadcasting participants count:', error);
+      }
+    }
+  });
+}
 
-  console.log(`New Connection to Channel: ${ws.channel}`);
+
 wsssl.on('connection', (ws, req) => {
   const channel = req.url.split('/').pop();
 
