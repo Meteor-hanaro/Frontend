40c7965add8b0ceb0d2902518c9e8ec2a70147a3
jiyeon <sjy6736@naver.com>
2024-06-11 17:14:41 +0900

    feat: 최종계약시 이미지 보이도록 수정

--- src/pages/consult/Sign.js
+++ src/pages/consult/Sign.js
@@ -5,6 +5,7 @@ import Pdf from '../../components/common/Pdf';
 import axios from 'axios';
 import TrafficChart from '../../components/common/chart/TrafficChart';
 import { Buffer } from 'buffer';
+import finalImage from '../../assets/final.png';
 
 const defaultStyle = {
   border: '1px solid gray',
@@ -50,7 +51,12 @@ function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomNum}) {
+function Sign({
+  suggestionItemData,
+  suggestionItemNumber,
+  suggestionId,
+  rtcRoomNum,
+}) {
   const ws = useRef(null);
   const [signatureCoordinates, setSignatureCoordinates] = useState(null);
   const [isDrawing, setIsDrawing] = useState(false);
@@ -58,6 +64,7 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
   const [pdfUrls, setPdfUrls] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [receivedPdfs, setReceivedPdfs] = useState([]);
+  const [showFinalImage, setShowFinalImage] = useState(false);
   const canvasRef = useRef(null);
   const array = useRef([]).current;
   const ctxRef = useRef(null);
@@ -66,7 +73,9 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
   const pbId = query.get('pbId');
   const vipId = query.get('vipId');
   useEffect(() => {
-    ws.current = new WebSocket(`ws://${process.env.REACT_APP_SUGGESTIONLISTWS}/${rtcRoomNum}`);
+    ws.current = new WebSocket(
+      `${process.env.REACT_APP_SUGGESTIONLISTWS}/${rtcRoomNum}`
+    );
     ws.current.onopen = () => {
       console.log('WebSocket connection opened');
     };
@@ -79,7 +88,7 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
           setReceivedPdfs(message.data);
         } else if (event.data instanceof Blob) {
           const reader = new FileReader();
-          reader.onload = function(loadEvent) {
+          reader.onload = function (loadEvent) {
             const imageDataUrl = loadEvent.target.result;
             const img = document.createElement('img');
             img.src = imageDataUrl;
@@ -112,24 +121,25 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
   }, []);
 
   const getPdf = () => {
-    axios.post(
-      `http://${process.env.REACT_APP_BESERVERURI}/api/contract/finalcontract`,
-      {
-        fundIds: suggestionItemData
-      }
-    )
-    .then((response) => {
-      const tmp = response.data.map((item, index) => ({
-        ...item.fundContracts[0],
-        id: index // assuming each fundContracts has a unique index
-      }));
-      setPdfUrls(tmp);
-      console.log('tmp확인', tmp);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  }
+    axios
+      .post(
+        `http://${process.env.REACT_APP_BESERVERURI}/api/contract/finalcontract`,
+        {
+          fundIds: suggestionItemData,
+        }
+      )
+      .then((response) => {
+        const tmp = response.data.map((item, index) => ({
+          ...item.fundContracts[0],
+          id: index, // assuming each fundContracts has a unique index
+        }));
+        setPdfUrls(tmp);
+        console.log('tmp확인', tmp);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
   const canvasEventListener = (event, type) => {
     if (!ctxRef.current) {
@@ -176,52 +186,56 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
     const image = canvas.toDataURL('image/png');
     setImageURL(image);
 
-    const updatedPdfUrls = await Promise.all(pdfUrls.map(async (pdfUrl) => {
-      const response = await fetch(pdfUrl.pdfUrl);
-      const pdfFile = await response.arrayBuffer();
-      const pdfDoc = await PDFDocument.load(pdfFile);
-      const page = pdfDoc.getPages()[0];
+    const updatedPdfUrls = await Promise.all(
+      pdfUrls.map(async (pdfUrl) => {
+        const response = await fetch(pdfUrl.pdfUrl);
+        const pdfFile = await response.arrayBuffer();
+        const pdfDoc = await PDFDocument.load(pdfFile);
+        const page = pdfDoc.getPages()[0];
 
-      const pngImageBytes = await fetch(image).then((res) => res.arrayBuffer());
-      const pngImage = await pdfDoc.embedPng(pngImageBytes);
+        const pngImageBytes = await fetch(image).then((res) =>
+          res.arrayBuffer()
+        );
+        const pngImage = await pdfDoc.embedPng(pngImageBytes);
 
-      const { width, height } = page.getSize();
-      const x = width - 150;
-      const y = height / 8 - 75;
+        const { width, height } = page.getSize();
+        const x = width - 150;
+        const y = height / 8 - 75;
 
-      if (signatureCoordinates) {
-        page.drawRectangle({
-          x: signatureCoordinates.x,
-          y: signatureCoordinates.y,
+        if (signatureCoordinates) {
+          page.drawRectangle({
+            x: signatureCoordinates.x,
+            y: signatureCoordinates.y,
+            width: 150,
+            height: 70,
+            color: rgb(1, 1, 1),
+            opacity: 1,
+          });
+        }
+
+        page.drawImage(pngImage, {
+          x,
+          y,
           width: 150,
           height: 70,
-          color: rgb(1, 1, 1),
-          opacity: 1,
         });
-      }
+        setSignatureCoordinates({ x, y });
+        const pdfBytes = await pdfDoc.save();
+        const base64pdf = Buffer.from(pdfBytes).toString('base64');
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+        console.log('url확인', url);
 
-      page.drawImage(pngImage, {
-        x,
-        y,
-        width: 150,
-        height: 70,
-      });
-      setSignatureCoordinates({ x, y });
-      const pdfBytes = await pdfDoc.save();
-      const base64pdf = Buffer.from(pdfBytes).toString('base64');
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-      const url = URL.createObjectURL(blob);
-      console.log("url확인", url);
-
-      return { ...pdfUrl, pdfUrl: url, signedContract: base64pdf };
-    }));
+        return { ...pdfUrl, pdfUrl: url, signedContract: base64pdf };
+      })
+    );
 
     setPdfUrls(updatedPdfUrls);
     clearCanvas();
     setShowModal(false);
     // WebSocket을 통해 싸인된 PDF 목록을 전송
     if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-      const signedPdfs = updatedPdfUrls.map(pdf => ({
+      const signedPdfs = updatedPdfUrls.map((pdf) => ({
         id: pdf.id,
         signedContract: pdf.signedContract,
       }));
@@ -241,20 +255,24 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
     const contracts = [];
     for (let i = 0; i < suggestionItemData.length; i++) {
       let pushItem = {
-        "fundId": suggestionItemData[i],
-        "signedContract": pdfUrls[i].signedContract
+        fundId: suggestionItemData[i],
+        signedContract: pdfUrls[i].signedContract,
       };
       contracts.push(pushItem);
     }
-    axios.post(`http://${process.env.REACT_APP_BESERVERURI}:8080/api/contract/signedcontract`,
-      {
-        suggestionId: suggestionId,
-        vipId: vipId,
-        pbId: pbId,
-        contracts: contracts
-      })
+    axios
+      .post(
+        `http://${process.env.REACT_APP_BESERVERURI}:8080/api/contract/signedcontract`,
+        {
+          suggestionId: suggestionId,
+          vipId: vipId,
+          pbId: pbId,
+          contracts: contracts,
+        }
+      )
       .then((response) => {
         alert('상품 가입이 완료되었습니다.');
+        setShowFinalImage(true);
         console.log(response);
       })
       .catch((error) => {
@@ -264,56 +282,62 @@ function Sign({ suggestionItemData, suggestionItemNumber, suggestionId, rtcRoomN
 
   return (
     <div id="signDiv">
-      <Modal show={showModal} handleClose={() => setShowModal(false)}>
-        <canvas
-          ref={canvasRef}
-          style={defaultStyle}
-          onMouseDown={(event) => {
-            canvasEventListener(event, 'down');
-          }}
-          onMouseMove={(event) => {
-            canvasEventListener(event, 'move');
-          }}
-          onMouseLeave={(event) => {
-            canvasEventListener(event, 'leave');
-          }}
-          onMouseUp={(event) => {
-            canvasEventListener(event, 'up');
-          }}
-        />
-        <button onClick={clearCanvas} className="btn btn-primary">
-          초기화
-        </button>
-        <button onClick={saveCanvas} className="btn btn-primary">
-          확인
-        </button>
-      </Modal>
-      <div id="finalPortfolio">
-        <div id="portfolioContainer">
-          <TrafficChart data={suggestionItemNumber} name='최종수정안' />
+      {showFinalImage ? (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <img src={finalImage} alt="Final" style={{ maxWidth: '60%' }} />
         </div>
-      </div>
-      <div id="finalContract">
-        <div>
-          <h3 className="final-title">최종계약서</h3>
-        </div>
-        <div id="finalPdf">
-          {
-            pdfUrls.map((data, index) => (
-              <div key={index}>
-                <h5 id="final-title">{data.title}</h5>
-                <Pdf pdfFile={data.pdfUrl} />
-              </div>
-            ))
-          }
-        </div>
-        <button onClick={signNow} className="btn-sign btn btn-primary">
-          서명
-        </button>
-        <button onClick={givePdf} className="btn-sign btn btn-primary">
-          최종가입
-        </button>
-      </div>
+      ) : (
+        <>
+          <Modal show={showModal} handleClose={() => setShowModal(false)}>
+            <canvas
+              ref={canvasRef}
+              style={defaultStyle}
+              onMouseDown={(event) => {
+                canvasEventListener(event, 'down');
+              }}
+              onMouseMove={(event) => {
+                canvasEventListener(event, 'move');
+              }}
+              onMouseLeave={(event) => {
+                canvasEventListener(event, 'leave');
+              }}
+              onMouseUp={(event) => {
+                canvasEventListener(event, 'up');
+              }}
+            />
+            <button onClick={clearCanvas} className="btn btn-primary">
+              초기화
+            </button>
+            <button onClick={saveCanvas} className="btn btn-primary">
+              확인
+            </button>
+          </Modal>
+          <div id="finalPortfolio">
+            <div id="portfolioContainer">
+              <TrafficChart data={suggestionItemNumber} name="최종수정안" />
+            </div>
+          </div>
+          <div id="finalContract">
+            <div>
+              <h3 className="final-title">최종계약서</h3>
+            </div>
+            <div id="finalPdf">
+              {pdfUrls.map((data, index) => (
+                <div key={index}>
+                  <h5 id="final-title">{data.title}</h5>
+                  <Pdf pdfFile={data.pdfUrl} />
+                </div>
+              ))}
+            </div>
+            <button onClick={signNow} className="btn-sign btn btn-primary">
+              서명
+            </button>
+            <button onClick={givePdf} className="btn-sign btn btn-primary">
+              최종가입
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
